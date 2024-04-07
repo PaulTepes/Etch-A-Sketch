817086526a652d7093c1ab97ebdc4b9c30a12d18
Paul Tepes <154736504+PaulTepes@users.noreply.github.com>
2024-04-07 14:00:12 +0700

    Add Ability to paint cells with random RGB colors
    Implemented generateRandomColor() function to
    generate random RGB colors.
    Updated RgbColorStyleChange() to use the generated
    random color. Now, clicking the RGB button paints
    cells with random colors.

--- script.js
+++ script.js
@@ -1,16 +1,79 @@
 document.addEventListener("DOMContentLoaded", function () {
   let mainContainer = document.querySelector("#main-container");
+  let newGridButton = document.querySelector("#new-grid");
+  let resetButton = document.querySelector("#reset");
+  let gridSize = 16;
 
-  for (let i = 1; i <= 16 * 16; i++) {
-    let cell = document.createElement("div");
-    cell.classList.add("cell");
-    mainContainer.appendChild(cell);
+  makeGrid(gridSize);
+  hoverCells();
+
+  resetButton.addEventListener("click", () => {
+    removeAllChildren(mainContainer);
+    gridSize = 16;
+    makeGrid(gridSize);
+    // hoverCells();
+  });
+
+  newGridButton.addEventListener("click", () => {
+    gridSize = prompt("Enter grid size from 1 to 100", "100");
+    removeAllChildren(mainContainer);
+    makeGrid(gridSize);
+  });
+
+  function removeAllChildren(parent) {
+    while (parent.firstChild) {
+      parent.removeChild(parent.firstChild);
+    }
   }
 
-  let cells = document.querySelectorAll(".cell");
-  cells.forEach((cell) =>
-    cell.addEventListener("mouseover", () => {
-      cell.classList.add("hovered-cell");
-    })
-  );
+  function makeGrid(gridSize) {
+    for (let i = 1; i <= gridSize ** 2; i++) {
+      let cell = document.createElement("div");
+      cell.classList.add("cell");
+      mainContainer.appendChild(cell);
+      hoverCells();
+    }
+  }
 });
+let rgbButton = document.querySelector("#rgb-switcher");
+let cells = document.querySelectorAll(".cell");
+
+function hoverCells() {
+  cells = document.querySelectorAll(".cell");
+  cells.forEach((cell) => {
+    cell.removeEventListener("mouseover", addHoverClass);
+    cell.removeEventListener("mouseover", RgbColorStyleChange);
+    cell.addEventListener("mouseover", addHoverClass);
+  });
+}
+
+function addHoverClass() {
+  this.classList.add("hovered-cell");
+}
+// RGB button + mouseover
+
+function RgbColorStyleChange() {
+  var randomColor = generateRandomColor();
+  this.style.backgroundColor = randomColor;
+}
+
+rgbButton.addEventListener("click", () => {
+  cells = document.querySelectorAll(".cell");
+  cells.forEach((cell) => {
+    cell.removeEventListener("mouseover", addHoverClass);
+    cell.removeEventListener("mouseover", RgbColorStyleChange);
+    cell.addEventListener("mouseover", RgbColorStyleChange);
+  });
+});
+
+//random generated color RGB
+function random255() {
+  return Math.floor(Math.random() * 256);
+}
+
+function generateRandomColor() {
+  var r = random255();
+  var g = random255();
+  var b = random255();
+  return `rgb(${r},${g},${b})`;
+}
